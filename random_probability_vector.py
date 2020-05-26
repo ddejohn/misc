2440034ce4798a2940b197fab29bfc0ea2150de4
Devon DeJohn <ddejohn@msudenver.edu>
2020-05-25 22:13:06 -0600

    messing around with markov chains

--- random_probability_vector.py
+++ random_probability_vector.py
@@ -1,9 +1,10 @@
-from random import randint
+from random import randint, choices, choice, seed
 
+seed(1)
 
 def prob_vec(n: int) -> list:
-    """Return a list of randomly generated non-zero
-       floats, the sum of which will be `1.0`.
+    """Return an `n`-length list of randomly generated
+       non-zero floats, the sum of which will be `1.0`.
     """
     T = 1000
     p = T
@@ -15,8 +16,14 @@ def prob_vec(n: int) -> list:
     return [*map(lambda num: round(num/T, 3), x)]
 
 
+def markov_step(w: list) -> int:
+    seq = [*range(len(w))]
+    x = choices(seq, w, k=len(w))
+    return choice(x)
+
 
 if __name__ == "__main__":
-    for _ in range(10):
-        x = prob_vec(10)
-        print(sum(x))
+    x = prob_vec(10)
+    m = [x[markov_step(x)] for _ in range(1000)]
+    for i in set(m):
+        print(f"{i}: {m.count(i)}")
