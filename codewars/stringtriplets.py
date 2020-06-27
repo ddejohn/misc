b9877147ea36ba6cf88261322f134f219b0a0ac3
Devon DeJohn <ddejohn@pm.me>
2020-06-27 15:47:47 -0600

    topological sort

--- codewars/stringtriplets.py
+++ codewars/stringtriplets.py
@@ -1,34 +1,31 @@
-# class Tree:
-#     def __init__(self, label):
-#         self.label = label
-#         self.nodes = []
+class DAG:
+    """Directed Acyclic Graph"""
+    def __init__(self, G):
+        self.G = G
+        self.visited = []
+        self.stack = []
 
-#     def __str__(self):
-#         return self.label + "\n  -> ".join(map(str, self.nodes))
-        # str_out = self.label
+    # helper function for topological sort
+    def top_help(self, vert):
+        self.visited.append(vert)
 
-        # if not self.nodes:
-        #     return str_out
+        for v in self.G.get(vert, []):
+            if v not in self.visited:
+                self.top_help(v)
         
-        # for branch in self.nodes:
-        #     str_out += f"\n  {branch}"
+        self.stack.insert(0, vert)
+    
+    # topological sort
+    def top_sort(self):
+        for v in self.G:
+            if v not in self.visited:
+                self.top_help(v)
         
-        # return str_out
-
-    # def flatten(self):
-    #     if not self.nodes:
-    #         return self.label
-        
-    #     path = [self.label]
-    #     for branch in self.nodes:
-
-
-
-# def get_key(d, v):
-#     return list(d.keys())[list(d.values()).index(v)]
+        return "".join(self.stack)
 
 
 def duplets(triplets):
+    """Generate a list of 'edges' from the triplets"""
     dups = []
     for tup in triplets:
         for i in [0,1]:
@@ -39,43 +36,11 @@ def duplets(triplets):
 
 
 def recoverSecret(triplets):
-    tree = {}
-    roots, nodes = set(), set()
-    for root, node in duplets(triplets):
-        roots.add(root)
-        nodes.add(node)
-        tree[root] = tree.get(root, []) + [node]
-
-    a, b = roots ^ nodes
-    start, end = (a, b) if a in tree else (b, a)
-    tree[end] = []
-
-    return top_sort(tree)
-    # print(tree)
-
-def top_sort(graph):
-    path = []
-    visited = []
-    temp = []
-    nodes = [*graph.keys()]
-    
-    def visit(n):
-        if n in visited:
-            return
-
-        temp.append(n)
-        for m in graph[n]:
-            visit(m)
-
-        temp.remove(n)
-        visited.append(n)
-        path = [n] + path
-
-    while nodes:
-        n = nodes.pop()
-        visit(n)
-    
-    return path
+    graph = {}
+    for a, b in duplets(triplets):
+        graph[a] = graph.get(a, []) + [b]
+    G = DAG(graph)
+    return G.top_sort()
 
 
 triplets = [
