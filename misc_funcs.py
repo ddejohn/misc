23bbebd96dffb3d95cbe9fdff243e2edf5e37e6b
Devon DeJohn <ddejohn@pm.me>
2020-07-04 01:53:57 -0600

    minor fix to anagrams

--- misc_funcs.py
+++ misc_funcs.py
@@ -9,6 +9,11 @@ def bits(n: int):
         yield choice([0,1])
 
 
+def rec_dig_sum(n: int) -> int:
+    q, m = divmod(n, 10)
+    return n if n == m else rec_dig_sum(q + rec_dig_sum(m))
+
+
 def fac(n: int) -> int:
     """Returns n!"""
     return 1 if n < 2 else n*fac(n-1)
@@ -31,8 +36,8 @@ def binomial_cdf(n: int, p: float, k: int) -> float:
 
 def anagrams(words: list) -> list:
     """Returns a list of words with anagrams present in input."""
-    scram = [*map(set, words)]
-    return [*filter(lambda w: scram.count(set(w)) > 1, words)]
+    scram = [*map(sorted, words)]
+    return [*filter(lambda w: scram.count(sorted(w)) > 1, words)]
 
 
 def sort_words(s: str) -> str:
@@ -49,8 +54,8 @@ def sort_words(s: str) -> str:
 
 
 # our test string
-# s = "4of Fo1r pe6ople (*) aofwj1 g3ood!e the2 st8 wan2 0a ssss aaa0a"
-# print(sort_words(s))
+s = "4of Fo1r pe6ople (*) aofwj1 g3ood!e the2 st8 wan2 0a ssss aaa0a"
+print(sort_words(s))
 
-# print(anagrams(["abcdcba", "radar", "cow", "bat", "tab"]))
+print(anagrams(["arrda", "radar", "cow", "bat", "tab", "batt"]))
 
