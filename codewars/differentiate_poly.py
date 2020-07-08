e9de522af415ee5a8523eaae5415c2233b50629a
Devon DeJohn <ddejohn@pm.me>
2020-07-08 02:53:47 -0600

    don't need to match constants

--- codewars/differentiate_poly.py
+++ codewars/differentiate_poly.py
@@ -1,10 +1,12 @@
 import re
 
-pattern = re.compile(r"(-?\d*)x\^?(\d*)|(-?\d*)")
+pattern = re.compile(r"(-?\d*)x\^?(\d*)")
 
 polys = [
     "3x^4+9x^3+3x-7",
     "-5x^2+10x+4",
+    "12x+2",
+    "x^2-x",
     "x+1"
 ]
 
