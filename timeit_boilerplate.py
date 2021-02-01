a6249edf62273c93269d5b689e9a7755decd007e
Devon DeJohn <ddejohn@pm.me>
2021-02-01 14:01:17 -0700

    backgammon started

--- timeit_boilerplate.py
+++ timeit_boilerplate.py
@@ -24,17 +24,34 @@ import timeit
 
 # s = "".join(choice(whitespace + punctuation) for _ in range(100000))
 
+def rec_fib(n: int) -> int:
+    if n in [0, 1, 2]:
+        return [0, 1, 1][n]
+    return rec_fib(n - 1) + rec_fib(n - 2)
+
+
+fib_lookup = {0: 0, 1: 1, 2: 1}
+
+
+def rec_fib_memo(n: int) -> int:
+    if n in fib_lookup:
+        return fib_lookup[n]
+    else:
+        fib_lookup[n] = rec_fib_memo(n - 1) + rec_fib_memo(n - 2)
+    return fib_lookup[n]
+
+
 t1 = timeit.timeit(
-    "[num for num in range(1000)]",
-    number=100000,
+    "rec_fib(40)",
+    number=10,
     globals=globals()
 )
 
 t2 = timeit.timeit(
-    "[*range(1000)]",
-    number=100000,
+    "rec_fib_memo(40)",
+    number=10,
     globals=globals()
 )
 
-print(f"comprehension: {t1:.2f} s")
-print(f"unpacking: {t2:.2f} s")
+print(f"non-memoized: {t1:.2f} s")
+print(f"memoized: {t2:.2f} s")
